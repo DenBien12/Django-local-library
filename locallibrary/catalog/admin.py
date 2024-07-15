489189bbf69a69597256e1193580d936dd04e42c
Dang Le Hai <haidangle678@gmail.com>
2024-07-15 23:28:09 +0700

    Updates models and admin

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Author, Book, BookInstance, Language, Genre
 
-# Register your models here.
+#admin.site.register(Book)
+#admin.site.register(Author)
+admin.site.register(Genre)
+#admin.site.register(BookInstance)
+admin.site.register(Language)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name','first_name', 'date_of_birth', 'date_of_death')
+
+admin.site.register(Author, AuthorAdmin)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
