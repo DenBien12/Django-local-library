21b1eeb60cc077fc51bc69c3a2816546049bccf9
Dang Le Hai <haidangle678@gmail.com>
2024-07-16 20:58:52 +0700

    Updates admins

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -10,13 +10,29 @@ admin.site.register(Language)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name','first_name', 'date_of_birth', 'date_of_death')
 
+    fields = ['first_name', 'last_name',('date_of_birth', 'date_of_death')]
 admin.site.register(Author, AuthorAdmin)
 
 
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    def get_extra(self, request, obj=None, **kwargs):
+        return 0 if obj else 1
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('status', 'due_back')
+    list_display = ('id','book', 'status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability',{
+            'fields': ('status', 'due_back')
+        })
+    )
