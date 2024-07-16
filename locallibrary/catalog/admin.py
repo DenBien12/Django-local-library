905427866b16cfa79da71171dd962404c76a0855
Dang Le Hai <haidangle678@gmail.com>
2024-07-16 21:03:15 +0700

    Updates admins

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -7,12 +7,15 @@ admin.site.register(Genre)
 #admin.site.register(BookInstance)
 admin.site.register(Language)
 
+class BookInline(admin.TabularInline):
+    model = Book
+
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name','first_name', 'date_of_birth', 'date_of_death')
 
     fields = ['first_name', 'last_name',('date_of_birth', 'date_of_death')]
-admin.site.register(Author, AuthorAdmin)
-
+    inlines = [BookInline]
 
 class BookInstanceInline(admin.TabularInline):
     model = BookInstance
